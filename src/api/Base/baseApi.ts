2abf985a87c4430e86add72e096452688fcd6cec
Daniyar <denaeshev@gmail.com>
2024-07-17 16:03:11 +0300

    fix url

--- src/api/Base/baseApi.ts
+++ src/api/Base/baseApi.ts
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
     baseQuery: fetchBaseQuery({
-        baseUrl: import.meta.env.VITE_BASE_API_URL,
+        baseUrl: "https://qr.konkov2024.ru",
         credentials: "include",
     }),
     reducerPath: "baseApi",
